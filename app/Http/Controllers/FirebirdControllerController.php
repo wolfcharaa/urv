3d621bb333439050b7c05d6098b09c68c8eec0dd
Алексей Михайлов <amikhailov@myelsa.ru>
2022-09-11 20:05:15 +0300

    Изменён FirebirdControllerController

--- app/Http/Controllers/FirebirdControllerController.php
+++ app/Http/Controllers/FirebirdControllerController.php
@@ -2,11 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\FirebirdController;
 use Illuminate\Http\Request;
 
 class FirebirdControllerController extends Controller
 {
-    public function createOrUpdate(Request $request) {
+    public function create(Request $request)
+    {
+        $data = $request->toArray();
+        $firebirdController = new FirebirdController();
+        $firebirdController->mac = $data['mac'];
+        $firebirdController->status = $data['status'];
+        $firebirdController->save();
+    }
 
+    public function update(int $id, Request $request)
+    {
+        /** @var FirebirdController $firebirdController */
+        $firebirdController = FirebirdController::query()
+            ->find($id);
+        $requestData = $request->toArray();
+        $firebirdController->mac = $requestData['mac'];
+        $firebirdController->save();
     }
 }
